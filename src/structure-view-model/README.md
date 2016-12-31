c02cf1088403283c18185254c36d85aa1ccd251c
Reinhold Früsmer <reinhold.fruesmer@tt-s.com>
2016-12-31 20:04:46 +0000

    Removed subtree sources

--- src/structure-view-model/README.md
+++
@@ -1,1 +0,0 @@
-# module-structure-view-model
--- src/structure-view-model/structure-view-model.ts
+++
@@ -1,8 +0,0 @@
-import {StructureViewModelNode} from "./structure-view-model-node";
-import {StructureViewModelDependency} from "./structure-view-model-dependency";
-
-
-export class StructureViewModel {
-    root: StructureViewModelNode = null;
-    dependencies: Array<StructureViewModelDependency> = [];
-}
