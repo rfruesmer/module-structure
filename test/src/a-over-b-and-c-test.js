96b4fc08b0bf67ea0f18440730531ac29d6786a9
Reinhold Früsmer <rfruesmer@gmail.com>
2017-01-02 06:53:41 +0100

    Made test parameterizable

---
+++ test/src/a-over-b-and-c-test.js
@@ -0,0 +1,90 @@
+const describe = require("mocha").describe;
+const it = require("mocha").it;
+
+const util = require("./module-structure-integration-test");
+const buildViewModelFor = util.buildViewModelFor;
+const expectRootIsPresent = util.expectRootIsPresent;
+const expectPackageNodeToEqual = util.expectPackageNodeToEqual;
+const expectRootRowCountToEqual = util.expectRootRowCountToEqual;
+const expectRowNodesCountToEqual = util.expectRowNodesCountToEqual;
+const expectRowContainsPackage = util.expectRowContainsPackage;
+const expectPackageContainsSingleModule = util.expectPackageContainsSingleModule;
+const expectDependencyCountToEqual = util.expectDependencyCountToEqual;
+const expectContainsDependency = util.expectContainsDependency;
+
+
+describe("a-over-b-and-c", function() {
+
+    const tests = [
+        {rootDir: "test/resources/es6/a-over-b-and-c", moduleType: "es6", extension: ".js"},
+        {rootDir: "test/resources/ts/a-over-b-and-c", moduleType: "ts", extension: ".ts"}
+    ];
+
+    tests.forEach(test => {
+        test.viewModel = buildViewModelFor(test.rootDir, test.moduleType);
+    });
+
+    tests.forEach(function(test) {
+        describe(test.moduleType, function() {
+
+            it("contains root", function () {
+                expectRootIsPresent(test.viewModel);
+            });
+
+            it("root is valid", function () {
+                expectPackageNodeToEqual(test.viewModel.root, "a-over-b-and-c", "a-over-b-and-c");
+            });
+
+            it("root contains two rows", function () {
+                expectRootRowCountToEqual(test.viewModel, 2);
+            });
+
+            it("package-a should be in first row", function () {
+                const row = test.viewModel.root.rows[0];
+                expectRowNodesCountToEqual(row, 1);
+                expectRowContainsPackage(row, "a-over-b-and-c.package-a", "package-a");
+            });
+
+            it("package-a contains module-a", function () {
+                expectPackageContainsSingleModule(test.viewModel, "a-over-b-and-c.package-a", 0, "module-a" + test.extension);
+            });
+
+            it("second row contains two elements", function () {
+                const row = test.viewModel.root.rows[1];
+                expectRowNodesCountToEqual(row, 2);
+            });
+
+            it("second row contains package-b", function () {
+                const row = test.viewModel.root.rows[1];
+                expectRowContainsPackage(row, "a-over-b-and-c.package-b", "package-b");
+            });
+
+            it("package-b contains module-b", function () {
+                expectPackageContainsSingleModule(test.viewModel, "a-over-b-and-c.package-b", 1, "module-b" + test.extension);
+            });
+
+            it("second row contains package-c", function () {
+                const row = test.viewModel.root.rows[1];
+                expectRowContainsPackage(row, "a-over-b-and-c.package-c", "package-c");
+            });
+
+            it("package-c contains module-c", function () {
+                expectPackageContainsSingleModule(test.viewModel, "a-over-b-and-c.package-c", 1, "module-c"  + test.extension);
+            });
+
+            it("contains two dependencies", function () {
+                expectDependencyCountToEqual(test.viewModel, 2);
+            });
+
+            it("contains dependency from module-a to module-b", function () {
+                expectContainsDependency(test.viewModel, "a-over-b-and-c.package-a.module-a" + test.extension,
+                    "a-over-b-and-c.package-b.module-b" + test.extension);
+            });
+
+            it("contains dependency from module-a to module-c", function () {
+                expectContainsDependency(test.viewModel, "a-over-b-and-c.package-a.module-a" + test.extension,
+                    "a-over-b-and-c.package-c.module-c" + test.extension);
+            });
+        });
+    });
+});
---
+++ test/src/module-structure-integration-test.ts
@@ -0,0 +1,89 @@
+import {StructureMapBuilder} from "../../src/structure-map/structure-map-builder";
+import {StructureViewModelBuilder} from "../../src/structure-map/structure-view-model-builder";
+import {StructureViewModel} from "../../src/structure-view-model/structure-view-model";
+import {expect} from "chai";
+import {StructureViewModelNode} from "../../src/structure-view-model/structure-view-model-node";
+
+
+export function buildViewModelFor(rootDir: string, moduleType: string): StructureViewModel {
+    let structureMap = new StructureMapBuilder().build(rootDir, moduleType);
+    let viewModel = new StructureViewModelBuilder().build(structureMap);
+    expect(viewModel).to.exist;
+
+    return viewModel;
+}
+
+export function expectRootIsPresent(viewModel) {
+    expect(viewModel).to.have.property("root");
+}
+
+export function expectRootRowCountToEqual(viewModel: StructureViewModel, expectedRowCount: number) {
+    expect(viewModel.root).to.have.property("rows").with.length(expectedRowCount);
+}
+
+export function expectRowNodesCountToEqual(row: Array<StructureViewModelNode>, expectedNodesCount: number) {
+    expect(row).to.have.length(expectedNodesCount);
+}
+
+export function expectRowContainsPackage(row: Array<StructureViewModelNode>, packageId: string, packageName: string): void {
+    let packageNode = this.findNode(packageId, row);
+    this.expectPackageNodeToEqual(packageNode, packageId, packageName);
+}
+
+export function findNode(id: string, row: Array<StructureViewModelNode>): StructureViewModelNode {
+    let searchResult = row.filter(entity => entity.id === id);
+    expect(searchResult).to.have.length(1);
+
+    return searchResult[0];
+}
+
+export function expectRowContainsModule(row: Array<StructureViewModelNode>, moduleId: string, moduleName: string) {
+    let node = this.findNode(moduleId, row);
+    expect(node).to.be.an.instanceOf(StructureViewModelNode);
+    expect(node.id).to.equal(moduleId);
+    expect(node.name).to.equal(moduleName);
+    expect(node.isGroup).to.equal(false);
+}
+
+export function expectPackageNodeToEqual(packageNode: StructureViewModelNode, packageId: string, packageName: string): void {
+    expect(packageNode).to.be.an.instanceOf(StructureViewModelNode);
+    expect(packageNode.id).to.equal(packageId);
+    expect(packageNode.name).to.equal(packageName);
+    expect(packageNode.isGroup).to.equal(true);
+}
+
+export function expectPackageContainsSingleModule(viewModel: StructureViewModel, packageId: string, packageRow: number, moduleName: string): void {
+    let row = viewModel.root.rows[packageRow];
+    let node = this.findNode(packageId, row);
+
+    expect(node.rows).to.have.length(1);
+
+    row = node.rows[0];
+    expect(row).to.have.length(1);
+
+    node = row[0];
+
+    this.expectModuleNodeToEqual(node, packageId + "." + moduleName, moduleName);
+}
+
+export function expectModuleNodeToEqual(moduleNode: StructureViewModelNode, moduleId: string, moduleName: string): void {
+    expect(moduleNode).to.be.an.instanceOf(StructureViewModelNode);
+    expect(moduleNode.id).to.equal(moduleId);
+    expect(moduleNode.name).to.equal(moduleName);
+    expect(moduleNode.isGroup).to.equal(false);
+    expect(moduleNode.rows).to.have.length(0);
+}
+
+export function expectNodeRowsCountToEqual(node: StructureViewModelNode, expectedRowsCount: number) {
+    expect(node.rows).to.have.length(expectedRowsCount);
+}
+
+export function expectDependencyCountToEqual(viewModel: StructureViewModel, expectedCount: number) {
+    expect(viewModel).to.have.property("dependencies");
+    expect(viewModel.dependencies).to.have.length(expectedCount);
+}
+
+export function expectContainsDependency(viewModel: StructureViewModel, from: string, to: string): void {
+    let searchResult = viewModel.dependencies.filter(dependency => dependency.from === from && dependency.to === to);
+    expect(searchResult).to.have.length(1);
+}
