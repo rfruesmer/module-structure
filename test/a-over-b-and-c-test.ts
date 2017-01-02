e318d961f75741bfe66799fd6c3dfd48747c4deb
Reinhold Früsmer <rfruesmer@gmail.com>
2017-01-02 02:24:22 +0100

    Refactored tests

--- test/a-over-b-and-c-test.ts
+++ test/a-over-b-and-c-test.ts
@@ -9,51 +9,36 @@ import {StructureViewModelBuilder} from "../src/structure-map/structure-view-mod
 @suite class AOverBAndCTest extends ModuleStructureIntegrationTest {
 
     before() {
-        let structureMap = new StructureMapBuilder().build("test/resources/ts/a-over-b-and-c");
-        this.viewModel = new StructureViewModelBuilder().build(structureMap);
+        this.buildViewModelFor("test/resources/ts/a-over-b-and-c");
     }
 
     @test "contains root"() {
-        expect(this.viewModel).to.have.property("root");
+        this.expectRootIsPresent();
     }
 
     @test "root is valid"() {
-        expect(this.viewModel.root).to.be.an.instanceOf(StructureViewModelNode);
-        expect(this.viewModel.root.id).to.equal("a-over-b-and-c");
-        expect(this.viewModel.root.name).to.equal("a-over-b-and-c");
-        expect(this.viewModel.root.isGroup).to.equal(true);
+        this.expectPackageNodeToEqual(this.viewModel.root, "a-over-b-and-c", "a-over-b-and-c");
     }
 
     @test "root contains two rows"() {
-        expect(this.viewModel.root).to.have.property("rows").with.length(2);
+        this.expectRootRowCountToEqual(2);
     }
 
     @test "package-a should be in first row"() {
-        let firstRow = this.viewModel.root.rows[0];
-        expect(firstRow).to.have.length(1);
+        let row = this.viewModel.root.rows[0];
 
-        let viewModelNode = firstRow[0];
-        expect(viewModelNode).to.be.an.instanceOf(StructureViewModelNode);
-        expect(viewModelNode.id).to.equal("a-over-b-and-c.package-a");
-        expect(viewModelNode.name).to.equal("package-a");
-        expect(viewModelNode.isGroup).to.equal(true);
+        this.expectRowNodesCountToEqual(row, 1);
+        this.expectRowContainsPackage(row, "a-over-b-and-c.package-a", "package-a");
     }
 
-    @test "package-a contains class-a"() {
-        let row = this.viewModel.root.rows[0];
-        let packageNode = row[0];
-
-        expect(packageNode.rows).to.have.length(1);
-
-        row = packageNode.rows[0];
-        expect(row).to.have.length(1);
-
-        this.expectModuleNodeToEqual(row[0], "a-over-b-and-c.package-a.class-a.ts", "class-a.ts");
+    @test "package-a contains module-a"() {
+        this.expectPackageContainsSingleModule("a-over-b-and-c.package-a", 0, "module-a.ts");
     }
 
     @test "second row contains two elements"() {
         let row = this.viewModel.root.rows[1];
-        expect(row).to.have.length(2);
+
+        this.expectRowNodesCountToEqual(row, 2);
     }
 
     @test "second row contains package-b"() {
@@ -61,16 +46,8 @@ import {StructureViewModelBuilder} from "../src/structure-map/structure-view-mod
         this.expectRowContainsPackage(row, "a-over-b-and-c.package-b", "package-b");
     }
 
-    @test "package-b contains class-b"() {
-        let row = this.viewModel.root.rows[1];
-        let packageNode = this.findNode("a-over-b-and-c.package-b", row);
-
-        expect(packageNode.rows).to.have.length(1);
-
-        row = packageNode.rows[0];
-        expect(row).to.have.length(1);
-
-        this.expectModuleNodeToEqual(row[0], "a-over-b-and-c.package-b.class-b.ts", "class-b.ts");
+    @test "package-b contains module-b"() {
+        this.expectPackageContainsSingleModule("a-over-b-and-c.package-b", 1, "module-b.ts");
     }
 
     @test "second row contains package-c"() {
@@ -78,28 +55,19 @@ import {StructureViewModelBuilder} from "../src/structure-map/structure-view-mod
         this.expectRowContainsPackage(row, "a-over-b-and-c.package-c", "package-c");
     }
 
-    @test "package-c contains class-c"() {
-        let row = this.viewModel.root.rows[1];
-        let packageNode = this.findNode("a-over-b-and-c.package-c", row);
-
-        expect(packageNode.rows).to.have.length(1);
-
-        row = packageNode.rows[0];
-        expect(row).to.have.length(1);
-
-        this.expectModuleNodeToEqual(row[0], "a-over-b-and-c.package-c.class-c.ts", "class-c.ts");
+    @test "package-c contains module-c"() {
+        this.expectPackageContainsSingleModule("a-over-b-and-c.package-c", 1, "module-c.ts");
     }
 
     @test "contains two dependencies"() {
-        expect(this.viewModel).to.have.property("dependencies");
-        expect(this.viewModel.dependencies).to.have.length(2);
+        this.expectDependencyCountToEqual(2);
     }
 
-    @test "contains dependency from class-a to class-b"() {
-        this.expectContainsDependency("a-over-b-and-c.package-a.class-a.ts", "a-over-b-and-c.package-b.class-b.ts");
+    @test "contains dependency from module-a to module-b"() {
+        this.expectContainsDependency("a-over-b-and-c.package-a.module-a.ts", "a-over-b-and-c.package-b.module-b.ts");
     }
 
-    @test "contains dependency from class-a to class-c"() {
-        this.expectContainsDependency("a-over-b-and-c.package-a.class-a.ts", "a-over-b-and-c.package-c.class-c.ts");
+    @test "contains dependency from module-a to module-c"() {
+        this.expectContainsDependency("a-over-b-and-c.package-a.module-a.ts", "a-over-b-and-c.package-c.module-c.ts");
     }
 }
