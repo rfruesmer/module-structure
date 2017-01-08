a2fe1c3d34b3c97d1a916bd6efeb6fd72d61b77b
Reinhold Früsmer <rfruesmer@gmail.com>
2017-01-08 18:49:29 +0100

    Fixed index.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-// This file is just added for convenience so this repository can be
-// directly checked out into a project's deps folder
-module.exports = require("./lib/main");
+module.exports = require("./dist/lib/module-structure");
